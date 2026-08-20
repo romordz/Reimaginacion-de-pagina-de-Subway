59587621a1188d900970046f06a23fb87486a5e8
romordz <rickyromo32@hotmail.com>
2026-08-20 16:12:32 -0600

    Refactor navbar layout by adjusting user dropdown structure and improving alignment of shopping cart button

--- Visual/navbar.php
+++ Visual/navbar.php
@@ -59,13 +59,13 @@
             </div>
         </div>
         <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <div class="d-flex flex-row">
-                <div class="dropdown logged-user sixth">
-                    <button class="btn tabs logged-user" type="button" data-bs-toggle="offcanvas"
+            <div class="d-flex flex-row align-items-center">
+                <button class="btn tabs logged-user" type="button" data-bs-toggle="offcanvas"
                         data-bs-target="#ShoppingCart" aria-controls="ShoppingCart">
                         <i class="bi bi-cart-fill fs-1"></i>
                         <span id="ElementosCarrito" class="badge">0</span>
                     </button>
+                <div class="dropdown logged-user sixth">
                     <button
                         class="btn tabs username h-100 d-flex flex-row justify-content-center align-items-center text-center"
                         type="button" data-bs-toggle="dropdown" aria-expanded="false">
