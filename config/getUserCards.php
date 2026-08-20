5a2bfbcdb0dd52c78df311f2a1ebd7c3960a3ab7
romordz <rickyromo32@hotmail.com>
2026-08-20 15:50:18 -0600

    Clean up getUserCards.php by removing commented code and unnecessary whitespace

--- config/getUserCards.php
+++ config/getUserCards.php
@@ -3,14 +3,8 @@ include('db.php');
 header('Content-Type: application/json');
 session_start();
 
-// Checar si usuario esta en sesion
 if (isset($_SESSION['id']) && isset($_SESSION['Nombre'])) {
-    // Usuario esta en sesion
     $id =  $_SESSION['id'];
-
-
-
-
     $sql = "SELECT * FROM `credit-cards` WHERE id_user = '$id'";
     $result = $conn->query($sql);
 
@@ -20,12 +14,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['Nombre'])) {
             $data[] = $row;
         }
     }
-
     echo json_encode($data);
-
-    $conn->close();
 } else {
-    // Usuario no esta en sesion
     header('Content-Type: application/json');
     echo json_encode(array('success' => false, 'message' => 'User not logged in.'));
 }
